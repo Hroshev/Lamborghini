26039d25e1333adb0dc0e5b6c24f100eb3c28ec3
unknown <grosheff.ivan@gmail.com>
2023-01-29 13:34:14 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import * as burger from "./modules/burger.js"
+
+// import * as popup from "./modules/popup.js"
+// import * as regularExpressions from "./modules/regularExpressions.js"
+// import * as accordion from "./modules/accordion.js"
+// import * as swiper from "./modules/swiper.js"
+// import * as products from "./modules/products.js"
+// import * as date from "./modules/date.js"
