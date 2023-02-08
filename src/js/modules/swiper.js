b4c59ffc7618e9e1f126340bc56bd05f4bd5fd26
unknown <grosheff.ivan@gmail.com>
2023-02-08 23:19:17 +0200

    added adaptive

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -2,10 +2,20 @@ import Swiper from "swiper/bundle";
 
 const swiper = new Swiper(".blog__slider", {
   loop: true,
-  slidesPerView: 2,
+  slidesPerView: 1,
   spaceBetween: 20,
   navigation: {
     nextEl: ".swiper-button-rigth",
     prevEl: ".swiper-button-left",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // 992: {
+    //   slidesPerView: 4,
+    //   spaceBetween: 30,
+    // },
   },
 });
